b64aa31ff4e6a30f2103081be76f3bdff82c0e6d
dai040520033 <86766722+dai040520033@users.noreply.github.com>
2021-08-04 19:44:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Chào cậu nhen!',
-    introDesc: `4 câu trắc nghiệm tớ có thể chọn sai.
-    Chứ giữa cậu với 7 tỉ người chắc chắn đúng =]]
-    Lạc vào tim cậu lẻ loi
-    Đằng sau chữ yêu đây chắc chắn là thương`,
-    btnIntro: ' Ấn Vào đây này ',
-    title: 'Làm thế nào để tớ bày tỏ cảm xúc với cậu đây 😙',
-    desc: 'Tớ không biết ngôn tình . Nhưng tớ biết tớ thích cậu ròii . Còn cậu, thì sao?',
-    btnYes: 'Em thích anhh <33',
-    btnNo: 'Em không thích anh :3',
-    question:'Cậu đồng ý rồi giờ là người yêu tớ đó nha <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'I LOVE YOU.',
+    introTitle: 'Hello!',
+    introDesc: `Chúc Thanh Thanh Một Ngày Tốt Lành`,
+    btnIntro: ' Tiếp Tục ',
+    title: 'Mày cứ bị đáng yếu ấyy!!',
+    desc: 'Giữa Người Mày Thích Và Người Thích Mày . Mày Sẽ Chọn?',
+    btnYes: 'Người Thích Mày ',
+    btnNo: 'Người Mày Thích :3',
+    question:'Tao Thích Mày . Cho Tao Cơ Hội Nhé!!😶',
+    btnReply: ' Được Chứ? <3',
+    reply: 'Được Được Được ',
+    mess: 'Thankiu Mylove 😘😘',
+    messDesc: ' LOVE YOU.',
     btnAccept: 'LOVE YOu <3',
     messLink: 'http://fb.com' 
 }
